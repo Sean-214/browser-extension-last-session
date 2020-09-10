c91875d6da56c51bfd83c9a32f785d708f18e93d
Sean Zhang <zhangxuan870214@163.com>
2020-09-10 12:51:12 +0800

    修复新打开浏览器时最近关闭列表无法获取图标的问题

--- src/lib/sessions.js
+++ src/lib/sessions.js
@@ -2,6 +2,8 @@ import storage from './storage';
 import util from './util';
 
 const LAST_SESSION = { lastSession: { lastModified: 0, tabs: [] } };
+const CACHED_RECENTS = { cachedRecents: {} };
+const EXPIRES = 7 * 24 * 60 * 60 * 1000;
 
 /**
  * 获取最近关闭的标签页和/或窗口的列表
@@ -48,16 +50,44 @@ async function setRemoved(url, removed) {
 
 async function getRecents(filter = null) {
   const tabs = [];
-  const sessionList = await getRecentlyClosed();
-  for (const session of sessionList) {
+  const sessions = await getRecentlyClosed();
+  const cachedRecents = await getCachedRecents();
+  const now = Date.now();
+  for (const session of sessions) {
     if (session && session.tab && session.tab.sessionId != null && !util.isInnerUrl(session.tab.url)) {
       tabs.push(session.tab);
+      // 从缓存中获取favIconUrl
+      const key = session.tab.url;
+      const favIconUrl = session.tab.favIconUrl;
+      if (favIconUrl) {
+        cachedRecents[key] = { favIconUrl, expires: now };
+      } else if (cachedRecents[key]) {
+        session.tab.favIconUrl = cachedRecents[key].favIconUrl;
+        // 更新缓存过期时间
+        cachedRecents[key].expires = now;
+      }
     }
   }
+  // 更新缓存
+  for (const [key, value] of Object.entries(cachedRecents)) {
+    if (now - value.expires > EXPIRES) {
+      delete cachedRecents[key];
+    }
+  }
+  await setCachedRecents(cachedRecents);
   if (filter && filter.maxResults) {
     return tabs.slice(0, filter.maxResults);
   }
   return tabs;
+}
+
+async function getCachedRecents() {
+  const items = await storage.get(CACHED_RECENTS);
+  return items.cachedRecents;
+}
+
+function setCachedRecents(cachedRecents = {}) {
+  return storage.set({ cachedRecents });
 }
 
 export default {
